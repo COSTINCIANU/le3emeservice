f94af42ed73cc962bfb944085dc007d57282e71f
COSTINCIANU <gheorghina.costincianu@sfr.fr>
2018-06-15 13:43:18 +0200

    table don de le3service

---
+++ src/Migrations/Version20180615102205.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180615102205 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE don ADD id_association INT NOT NULL, ADD id_professionnel INT NOT NULL, ADD description VARCHAR(255) NOT NULL, ADD date_entree DATETIME NOT NULL, ADD reserver VARCHAR(255) NOT NULL, ADD heure_collecte VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE don DROP id_association, DROP id_professionnel, DROP description, DROP date_entree, DROP reserver, DROP heure_collecte');
+    }
+}
